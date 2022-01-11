1caf7056fbf5f1c5c744b73f87cc2bcfafb46ae7
Anastacio Menezes <anastaciomenezes@outlook.com>
2022-01-10 23:44:59 -0300

    adicionando funcionalidade de check da tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,19 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { DeleteForever, Edit, CheckCircle, Cancel } from "@mui/icons-material/";
 
-
 function App() {
   const [task, setTask] = useState("");
   const [newTasks, setNewTasks] = useState([]);
   const [showAddAlert, setshowAddAlert] = useState(false);
   const [showRemoveAlert, setshowRemoveAlert] = useState(false);
-  const [taskEdited, setTaskEdited] = useState("")
-  
+  const [taskEdited, setTaskEdited] = useState("");
+
   useEffect(() => {
     setTimeout(() => {
       setshowAddAlert(false);
       setshowRemoveAlert(false);
     }, 3000);
   }, [showAddAlert, showRemoveAlert]);
-
 
   function handleClickKey(e) {
     if (e.key === "Enter") {
@@ -46,33 +44,44 @@ function App() {
 
   function handleClickEdit(id) {
     let selected = [...newTasks].find((task) => task.id === id);
-    if(selected){
-        selected.toEdit = true
-        setNewTasks([...newTasks])
-    }else{
-      selected.toEdit = false
+    if (selected) {
+      selected.toEdit = true;
+      setNewTasks([...newTasks]);
+    } else {
+      selected.toEdit = false;
     }
   }
 
-  function handleClickEditFalse(id){
+  function handleClickEditFalse(id) {
     let selected = [...newTasks].find((task) => task.id === id);
-    if(selected.toEdit){
-        selected.toEdit = false
-        setNewTasks([...newTasks])
-    }else{
-      selected.toEdit = true
+    if (selected.toEdit) {
+      selected.toEdit = false;
+      setNewTasks([...newTasks]);
+    } else {
+      selected.toEdit = true;
     }
   }
 
-  function handleClickEditTask(id){
+  function handleChecked(id) {
     let selected = [...newTasks].find((task) => task.id === id);
-    if(selected.description){
-        selected.description = taskEdited
-        selected.toEdit = false
-        setNewTasks([...newTasks])
-        setTaskEdited('')
-    }else{
-      selected.toEdit = true
+    if (selected.checked === false) {
+      selected.checked = true;
+      setNewTasks([...newTasks]);
+    } else {
+      selected.checked = false;
+      setNewTasks([...newTasks]);
+    }
+  }
+
+  function handleClickEditTask(id) {
+    let selected = [...newTasks].find((task) => task.id === id);
+    if (selected.description) {
+      selected.description = taskEdited;
+      selected.toEdit = false;
+      setNewTasks([...newTasks]);
+      setTaskEdited("");
+    } else {
+      selected.toEdit = true;
     }
   }
 
@@ -100,29 +109,47 @@ function App() {
           ) : (
             newTasks.map((task) => (
               <li id={task.id} key={task.id} className="taskItem">
-                {task.toEdit ? <div className="inputEdit">
-                  <input 
-                  type="text" 
-                  placeholder={task.description} 
-                  value={taskEdited} 
-                  onChange={e=>setTaskEdited(e.target.value)}/>
-                  <button onClick={()=>handleClickEditTask(task.id)}>Edit</button>
-                  <button onClick={()=>handleClickEditFalse(task.id)}>Back</button>
-                </div> : <>
-                  <p>
-                  {task.description.length > 20
-                    ? task.description.substring(0, 20) + "..."
-                    : task.description}
-                </p>
-                <div className="actions">
-                  <button onClick={() => handleClickDelete(task.id)}>
-                    <DeleteForever style={{ fontSize: 30, color: "#ff5b5b" }} />
-                  </button>
-                  <button onClick={() => handleClickEdit(task.id)}>
-                    <Edit style={{ fontSize: 30, color: "#5b9dff" }} />
-                  </button>
-                </div>
-                </>}
+                {task.toEdit ? (
+                  <div className="inputEdit">
+                    <input
+                      type="text"
+                      placeholder={task.description}
+                      value={taskEdited}
+                      onChange={(e) => setTaskEdited(e.target.value)}
+                    />
+                    <button onClick={() => handleClickEditTask(task.id)}>
+                      Edit
+                    </button>
+                    <button onClick={() => handleClickEditFalse(task.id)}>
+                      Back
+                    </button>
+                  </div>
+                ) : (
+                  <>
+                    <p
+                      onClick={() => handleChecked(task.id)}
+                      className={task.checked ? "isChecked " : ""}
+                    >
+                      {task.description.length > 20
+                        ? task.description.substring(0, 20) + "..."
+                        : task.description}
+                    </p>
+                    {!task.checked ? (
+                      <div className="actions">
+                        <button onClick={() => handleClickDelete(task.id)}>
+                          <DeleteForever
+                            style={{ fontSize: 30, color: "#ff5b5b" }}
+                          />
+                        </button>
+                        <button onClick={() => handleClickEdit(task.id)}>
+                          <Edit style={{ fontSize: 30, color: "#5b9dff" }} />
+                        </button>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </>
+                )}
               </li>
             ))
           )}
